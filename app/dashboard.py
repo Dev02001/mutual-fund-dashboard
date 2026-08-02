79dc395d18082bb1d57202d2fc2326ebc32c7ea1
Devashish Gurav <devagurav70@gmail.com>
2026-08-02 23:19:52 +0400

    After adding correct paths checking if dashboard is visble.

--- app/dashboard.py
+++ app/dashboard.py
@@ -2,49 +2,55 @@ import os
 import sys
 import streamlit as st
 
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
 st.write("Current working directory:", os.getcwd())
 st.write("Python path:", sys.path)
 st.title("Testing")
 
 #from analytics.curate_data import portfolio_summary
-#from analytics.demo_data import portfolio_summary_demo
+from analytics.demo_data import portfolio_summary_demo
 
 
 #portfolio = portfolio_summary("ICICI Prudential Dividend Yield Equity Mutual Fund Direct Growth")
-#portfolio = portfolio_summary_demo()
-#st.title("Mutual Fund Dashboard")
-#st.subheader(portfolio["Name"])
+portfolio = portfolio_summary_demo()
+st.title("Mutual Fund Dashboard")
+st.subheader(portfolio["Name"])
 
-#invest_column, current_column = st.columns(2)
+invest_column, current_column = st.columns(2)
 
-'''def money(value):
-    return f"₹{value:,.2f}"'''
+def money(value):
+    return f"₹{value:,.2f}"
 
-#with invest_column:
-    #st.metric(
-        #label= "Invested Value",
-        #value = money(portfolio['Amount_invested'])
-    #)
-#with current_column:
-    #st.metric(
-        #label= "Curent Market Value",
-        #value= money(portfolio['latest_value'])
-    #)
+with invest_column:
+    st.metric(
+        label= "Invested Value",
+        value = money(portfolio['Amount_invested'])
+    )
+with current_column:
+    st.metric(
+        label= "Curent Market Value",
+        value= money(portfolio['latest_value'])
+    )
 
-#profitLossColumn, absoluteReturn = st.columns(2)
+profitLossColumn, absoluteReturn = st.columns(2)
 
-#with profitLossColumn:
-    #st.metric(
-        #label="Profit/loss",
-        #value = money(portfolio['Profit and loss'])
-    #)
-#with absoluteReturn:
-    #st.metric(
-        #label= "Absolute Return",
-        #value=f"{portfolio['Absolute Return']}"
-    #)
+with profitLossColumn:
+    st.metric(
+        label="Profit/loss",
+        value = money(portfolio['Profit and loss'])
+    )
+with absoluteReturn:
+    st.metric(
+        label= "Absolute Return",
+        value=f"{portfolio['Absolute Return']}"
+    )
 
-'''latestNAV, latestNAVDate = st.columns(2)
+latestNAV, latestNAVDate = st.columns(2)
 
 with latestNAV:
     st.metric(
@@ -55,4 +61,4 @@ with latestNAVDate:
     st.metric(
         label="NAV as of:",
         value=f"{portfolio['Latest_NAV_date']}"
-    )'''
+    )
